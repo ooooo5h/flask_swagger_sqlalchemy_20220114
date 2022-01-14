eece5b39ac9324a99d51c3069a313a5f189a4e40
ooooo5h <ooooo5h91@gmail.com>
2022-01-14 17:06:02 +0900

    swagger문서 페이지 등록

--- server/app.py
+++ server/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_restful_swagger_2 import Api
+from flask_swagger_ui import get_swaggerui_blueprint
 
 from server.api.lecture.lecture import Lecture
 
@@ -18,5 +19,11 @@ def create_app(config_name):
     # api폴더에서 만든 User클래스를 가져다가 /user로 접속 가능하게 등록
     api.add_resource(User, '/user')
     api.add_resource(Lecture, '/lecture')
+    
+    # swagger 문서를 자동 생성
+    swagger_ui = get_swaggerui_blueprint('/api/docs', '/api/spec.json', config={'app_name' : 'my sns service'})
+    
+    # 플라스크 앱에, url에 swagger문서를 등록
+    app.register_blueprint(swagger_ui, url_prefix='/api/docs')
     
     return app
