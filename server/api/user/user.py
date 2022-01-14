f6098df7826f8161b70b9d0434cc42b5aa82f2f5
ooooo5h <ooooo5h91@gmail.com>
2022-01-14 17:15:35 +0900

    로그인/회원가입에도 문서 추가

--- server/api/user/user.py
+++ server/api/user/user.py
@@ -26,12 +26,46 @@ class User(Resource):
         return {
             '임시' : '사용자 정보 조회'
         }
+  
         
+    @swagger.doc({
+        'tags' : ['user'],  # 어떤 종류의 기능인지 분류
+        'description' : '로그인',
+        'parameters' : [
+            # dict로 파라미터들 명시
+        ],
+        'responses' : {
+            # 200일때의 응답 예시, 400일때의 예시 등
+            '200' : {
+                'description' : '로그인 성공',
+            },
+            '400' : {
+                'description' : '아이디 없는 상황'
+            }
+        }
+    })    
     def post(self):
         return {
             '임시' : '로그인 기능'
         }
-        
+    
+    
+    @swagger.doc({
+        'tags' : ['user'],  # 어떤 종류의 기능인지 분류
+        'description' : '회원가입',
+        'parameters' : [
+            # dict로 파라미터들 명시
+        ],
+        'responses' : {
+            # 200일때의 응답 예시, 400일때의 예시 등
+            '200' : {
+                'description' : '회원가입 성공!',
+            },
+            '400' : {
+                'description' : '이메일 중복으로 인해 회원 가입 실패'
+            }
+        }
+    })        
     def put(self):
         return {
             '임시' : '회원가입 기능'
