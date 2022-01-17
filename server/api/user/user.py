a902bf772758fd62ea4e873dc67683b963af68eb
전은형 <ooooo5h91@gmail.com>
2022-01-17 15:54:49 +0900

    PATCH 메쏘드 접속 열기 / swagger 문서

--- server/api/user/user.py
+++ server/api/user/user.py
@@ -292,7 +292,7 @@ class User(Resource):
         'description' : '회원 탈퇴',
         'parameters' : [
             {
-                'name' : 'usre_id',
+                'name' : 'user_id',
                 'description' : '몇 번 사용자를 지울건가요?',
                 'in' : 'query',
                 'type' : 'integer',
@@ -344,3 +344,37 @@ class User(Resource):
             'message' : '회원 삭제 수행 완료'
         }
     
+
+    @swagger.doc({
+    'tags' : ['user'],
+    'description' : '회원 정보 수정',
+    'parameters' : [
+        {
+            'name' : 'user_id',
+            'description' : '몇 번 사용자의 정보를 수정할지?',
+            'in' : 'formData',
+            'type' : 'integer',
+            'required' : True,
+        },
+        {
+            'name' : 'field',
+            'description' : '어느 항목을 변경할지?',
+            'in' : 'formData',
+            'type' : 'string',    # name / phone 둘 중 하나로 입력받자
+            'required' : True,
+        },
+    ],
+        'responses' : {
+            '200' : {
+                'description' : '회원정보 변경 성공'
+            },
+            '400' : {
+                'description' : '회원정보 변경 실패'
+            }
+        }
+    })
+    def patch(self):
+        """회원정보 수정"""
+        return{
+            '임시' : '회원정보 수정 임시'
+        }
