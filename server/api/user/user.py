7b7eaeacccfe891df4cfcbcf6066e8fd49ae1d66
서유림 <sur6977@naver.com>
2022-01-17 09:46:53 +0900

    로그인 기능에 사용할 파라미터들을 명시
    required=True는 첨부를 안하면 에러로 리턴되도록 자동으로 처리됨

--- server/api/user/user.py
+++ server/api/user/user.py
@@ -1,8 +1,17 @@
 # 사용자에 관련된 기능을 수행하는 파일
 # 메쏘드를 만들 때, get/post/put/patch/delete로 만들면, 알아서 메쏘드로 세팅되도록
 
-from flask_restful import Resource
+from urllib import request
+from flask_restful import Resource, reqparse
 from flask_restful_swagger_2 import swagger
+
+# 각각의 메쏘드 별로 파라미터를 받아보자
+
+# post메쏘드에서 사용할 파라미터
+post_parser = reqparse.RequestParser() # post로 들어오는 파라미터를 확인해볼 변수를 생성
+post_parser.add_argument('email', type=str, required=True, location='form')  # 파라미터의 이름/데이터 타입/필수여부/첨부된 곳을 기재해주면 파라미터를 자동으로 가져옴
+post_parser.add_argument('password', type=str, required=True, location='form')
+
 
 class User(Resource):
     
