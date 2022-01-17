2afa8be43d3f09b22d4a019e3b2e85441d167f31
전은형 <ooooo5h91@gmail.com>
2022-01-17 15:11:06 +0900

    모델클래스에, DATETIME 컬럼 추가
    - 기본값을 넣으려면, db.func.current_timestamp() 로 처리하자.

--- server/model/users.py
+++ server/model/users.py
@@ -1,5 +1,6 @@
 # 논리 DB my_sns의 users 테이블에 연결되는 클래스
 from server import db
+import datetime
 
 class Users(db.Model):
     
@@ -10,13 +11,14 @@ class Users(db.Model):
     
     # 2. 어떤 변수들이 있고, 그 변수들이 어떤 컬럼에 연결되는지 => 변수이름과 컬럼이름은 같아야함
     id = db.Column(db.Integer, primary_key=True)  # id컬럼은 int로 되어있고 기본키라고 명시
-    email = db.Column(db.String(50), nullable=False, default='이메일미입력') # email컬럼은 50자 문구, null허용, 이메일미입력이라는 기본값 있다고 명시
+    email = db.Column(db.String(50), nullable=False, default='이메일 미입력') # email컬럼은 50자 문구, null허용, 이메일미입력이라는 기본값 있다고 명시
     password = db.Column(db.String(50), nullable=False)
     name = db.Column(db.String(20), nullable=False)
     phone = db.Column(db.String(15))  # nullable의 기본값은 null허용이라서 nullable=False의 경우만 기재해줘도 됨
     
     birth_year = db.Column(db.Integer, nullable=False, default=1995)    
-
+    # created_at = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now()) # 일반 datetitme.datetime.now()를 하면 현재 작업중인 pc서버의 시간이 기록됨 => DB 현재시간 아님
+    created_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
     
     # 3. 객체를 가지고 실제 dict로 변환해주는 메쏘드 생성(응답을 내려주는 용도)
     def get_data_object(self):
