bdbdc0dd303dfbb993e707d609ddb4262a66ad50
전은형 <ooooo5h91@gmail.com>
2022-01-18 11:50:49 +0900

    오류 : AWS에 실제 파일 업로드 / 퍼블릭 권한 허용
     - 누구나 볼 수 있는 파일로 설정까지

--- server/api/user/user_profile_image.py
+++ server/api/user/user_profile_image.py
@@ -65,6 +65,12 @@ class UserProfileImage(Resource):
             
             # 파일 본문도 따로 불러내서 저장해보자 => 실제로 S3경로에 업로드하는 데 활용할꺼임
             file_body = file.stream.read()   # 올려줄 파일이 만들어짐
+            
+            # 어떤 버킷에 올려줄건지 설정 => 파일 업로드(put_object())까지 한 큐에 하도록 처리
+            aws_s3.Bucket(current_app.config['AWS_S3_BUCKET_NAME']).put_object(key=s3_file_path, Body=file_body)
+            
+            # 이 파일을 누구나 볼 수 있게 public으로 허용하는 작업 필요함 , s3_file_path(파일이름을 찾아서)
+            aws_s3.ObjectAcl(current_app.config['AWS_S3_BUCKET_NAME'], s3_file_path).put(ACL='public-read')
         
         return {
             '임시' : '사용자가 프사를 등록하는 기능'
