73d0a11472d12e212e0b0e56092bb5ffb71bcc45
전은형 <ooooo5h91@gmail.com>
2022-01-18 11:16:32 +0900

    파일을 파라미터로 받기
    추가적인 라이브러리(같이 설치되었음) 활용

--- server/api/user/user_profile_image.py
+++ server/api/user/user_profile_image.py
@@ -1,6 +1,15 @@
-from flask_restful import Resource
+import imp
+from flask_restful import Resource, reqparse
+from werkzeug.datastructures import FileStorage  # 파라미터로 파일을 받을 때 필요한 클래스
+
+put_parser = reqparse.RequestParser()
+
+# 파일을 받는 파라미터는 FileStorage, files에서, 추가행동 : append가 필요
+put_parser.add_argument('profile_image', type=FileStorage, required=True, location='files', action='append')
+put_parser.add_argument('user_id', type=int, required=True, location='form')
 
 class UserProfileImage(Resource):
+
     
     def put(self):
         return {
