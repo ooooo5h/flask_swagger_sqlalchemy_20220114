3943f5ff1901d53b5901610dc1e3790edb1ad84c
전은형 <ooooo5h91@gmail.com>
2022-01-18 11:51:51 +0900

    오류해결 ; key가 아니라 Key

--- server/api/user/user_profile_image.py
+++ server/api/user/user_profile_image.py
@@ -57,7 +57,7 @@ class UserProfileImage(Resource):
         # args['profile_image'] 는 list로 구성되어있는 경우가 많다(여러장이니까)
         
         for file in args['profile_image']:
-            print(file)  # file : 파일이름 / 실제 이미지들의 본문이 분리된 형태로 만들어져 있음
+            # file : 파일이름 / 실제 이미지들의 본문이 분리된 형태로 만들어져 있음
             
             # 파일 이름 저장  => S3 버킷에 저장될 경로 생성에 활용할꺼임
             print(file.filename)
@@ -67,7 +67,7 @@ class UserProfileImage(Resource):
             file_body = file.stream.read()   # 올려줄 파일이 만들어짐
             
             # 어떤 버킷에 올려줄건지 설정 => 파일 업로드(put_object())까지 한 큐에 하도록 처리
-            aws_s3.Bucket(current_app.config['AWS_S3_BUCKET_NAME']).put_object(key=s3_file_path, Body=file_body)
+            aws_s3.Bucket(current_app.config['AWS_S3_BUCKET_NAME']).put_object(Key=s3_file_path, Body=file_body)
             
             # 이 파일을 누구나 볼 수 있게 public으로 허용하는 작업 필요함 , s3_file_path(파일이름을 찾아서)
             aws_s3.ObjectAcl(current_app.config['AWS_S3_BUCKET_NAME'], s3_file_path).put(ACL='public-read')
