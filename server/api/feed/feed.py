e48fd2289841ca998636fe3cfc81c3697d016420
전은형 <ooooo5h91@gmail.com>
2022-01-18 15:02:57 +0900

    파라미터가 쭉 들어오면, 사용자 정보와 아마존 S3 접속 도구 설정

--- server/api/feed/feed.py
+++ server/api/feed/feed.py
@@ -1,9 +1,15 @@
+import boto3
+import time
+import os
+import hashlib
+
+from flask import current_app
 from flask_restful import Resource, reqparse
 from flask_restful_swagger_2 import swagger
 from werkzeug.datastructures import FileStorage
 
 from server import db
-from server.model import Feeds
+from server.model import Feeds, Users
 
 post_parser = reqparse.RequestParser()
 post_parser.add_argument('user_id', type=int, required=True, location='form')
@@ -70,8 +76,22 @@ class Feed(Resource):
         
         if args['feed_images']:   # 사진이 파라미터에 첨부되어있나요?
             
+            # 1 : 사용자가 누구인가? 
+            upload_user = Users.query.filter(Users.id == args['user_id']).first()
+            
+            # 2 : AWS 접속 도구 셋팅
+            aws_s3 = boto3.resource('s3',\
+                aws_access_key_id=current_app.config['AWS_ACCESS_KEY_ID'],\
+                aws_secret_access_key=current_app.config['AWS_SECRET_ACCESS_KEY'])          
+            
             for image in args['feed_images']:
                 # 첨부된 사진들을 AWS S3에 올려주기
+                
+                # 1 : 파일 이름은 중복을 피해서 재가공
+                # 사용자id 암호화하고, 현재 시간을 숫자로 표현해서 써먹기
+                
+                
+                # 2 : AWS S3에 파일 업로드
                 
                 # feed_images라는 테이블에 이 게시글의 사진으로 S3에 올라간 사진 주소를 등록해줘야함
                 pass
