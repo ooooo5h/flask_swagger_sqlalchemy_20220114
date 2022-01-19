a7732cf646f219afa499fbfbe606b87ff2c38686
전은형 <ooooo5h91@gmail.com>
2022-01-19 14:59:15 +0900

    댓글 삭제 및 댓글 목록 조회하기
    댓글 목록 조회하기는 토큰/파라미터가 없는 경우임

--- server/api/feed/feed_reply.py
+++ server/api/feed/feed_reply.py
@@ -15,6 +15,8 @@ put_parser = reqparse.RequestParser()
 put_parser.add_argument('feed_reply_id', type=int, required=True, location='form')
 put_parser.add_argument('content', type=str, required=True, location='form')
 
+delete_parser = reqparse.RequestParser()
+delete_parser.add_argument('feed_reply_id', type=int, required=True, location='args')
 
 class FeedReply(Resource):
 
@@ -114,8 +116,7 @@ class FeedReply(Resource):
                 'in': 'formData',
                 'type': 'string',
                 'required': True,
-            },      
-                                              
+            },                                                  
         ],
         'responses' : {
             '200' : {
@@ -152,7 +153,100 @@ class FeedReply(Resource):
             'message' : '댓글 수정 성공'
         }
         
-
-
-
+    @swagger.doc({
+        'tags' : ['feed/reply'],  
+        'description' : '달아둔 댓글 삭제',
+        'parameters' : [
+            {
+                'name': 'X-Http-Token',
+                'description': '사용자 구별 토큰',
+                'in': 'header',
+                'type': 'string',
+                'required': True,
+            },
+            {
+                'name': 'feed_id',
+                'description': '수정해줄 피드의 아이디',
+                'in': 'path',
+                'type': 'integer',
+                'required': True,
+            },             
+            {
+                'name': 'feed_reply_id',
+                'description': '몇 번 댓글을 삭제할건가요?',
+                'in': 'query',   # DELETE - query
+                'type': 'integer',
+                'required': True,
+            },                         
+        ],
+        'responses' : {
+            '200' : {
+                'description' : '댓글 삭제 성공!',
+            }
+        }
+    })     
+    @token_required      
+    def delete(self, feed_id):
+        """댓글 삭제"""
         
+        args = delete_parser.parse_args()
+        user = g.user
+        
+        # 기존 댓글을 확인해보기 => 내가 쓴 댓글이 맞는지
+        reply = FeedReplies.query.filter(FeedReplies.id == args['feed_reply_id']).first()
+        
+        if reply.user_id != user.id:
+            # 댓글의 작성자가 내가 아니다
+            return{
+                'code' : 400,
+                'message' : '본인이 쓴 댓글만 삭제할 수 있습니다.'
+            }, 400   
+        
+        # DB에 삭제 처리 => 변경 생김
+        db.session.delete(reply)
+        db.session.commit()
+        
+        return {
+            'code' : 200,
+            'message' : '댓글 삭제 성공'
+        }
+
+
+
+    @swagger.doc({
+        'tags' : ['feed/reply'],  
+        'description' : '특정 게시글에 달린 댓글 목록 보기',
+        'parameters' : [
+            {
+                'name': 'feed_id',
+                'description': '몇번 게시글에 달린 댓글 목록을 조회할건가요?',
+                'in': 'path',
+                'type': 'integer',
+                'required': True,
+            },                                                           
+        ],
+        'responses' : {
+            '200' : {
+                'description' : '댓글 목록 조회 성공!',
+            },
+        }
+    })  
+    def get(self, feed_id):
+        """게시글의 댓글 목록 조회"""
+        
+        # args = put_parser.parse_args()
+        # user = g.user
+        
+        reply_data_list = FeedReplies.query\
+            .filter(FeedReplies.feed_id == feed_id)\
+            .all()
+        
+        replies = [reply.get_data_object() for reply in reply_data_list]
+        
+        return {
+            'code' : 200,
+            'message' : '댓글 목록 조회 성공',
+            'data' : {
+                'replies' : replies
+            }
+        }
