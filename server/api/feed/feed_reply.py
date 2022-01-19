8aaad917a97c7c9718b8f479ceb6c29ee431a008
전은형 <ooooo5h91@gmail.com>
2022-01-19 12:24:25 +0900

    댓글 수정 기능의 문서/파라미터 작업

--- server/api/feed/feed_reply.py
+++ server/api/feed/feed_reply.py
@@ -11,6 +11,11 @@ post_parser = reqparse.RequestParser()
 post_parser.add_argument('feed_id', type=int, required=True, location='form')
 post_parser.add_argument('content', type=str, required=True, location='form')
 
+put_parser = reqparse.RequestParser()
+put_parser.add_argument('feed_reply_id', type=int, required=True, location='form')
+put_parser.add_argument('content', type=str, required=True, location='form')
+
+
 class FeedReply(Resource):
 
     @swagger.doc({
@@ -75,3 +80,51 @@ class FeedReply(Resource):
         }
 
     
+    # put 메쏘드 만들기 (타 클래스 참고 / 코드 재활용)
+    # 기존에 만들어진 댓글의 id를 받아서 수정처리
+    # 제약 사항 : 본인이 쓴 댓글만 실제로 수정할 수 있음. 타인이 쓴 댓글이라면 400으로 '본인이 쓴 댓글만 수정가능합니다.'
+    @swagger.doc({
+        'tags' : ['feed/reply'],  
+        'description' : '달아둔 댓글 수정',
+        'parameters' : [
+            {
+                'name': 'X-Http-Token',
+                'description': '사용자 구별 토큰',
+                'in': 'header',
+                'type': 'string',
+                'required': True,
+            },
+            {
+                'name': 'feed_reply_id',
+                'description': '몇번 댓글을 수정할건지',
+                'in': 'formData',
+                'type': 'integer',
+                'required': True,
+            },
+            {
+                'name': 'content',
+                'description': '수정해줄 댓글 내용',
+                'in': 'formData',
+                'type': 'string',
+                'required': True,
+            },                                       
+        ],
+        'responses' : {
+            '200' : {
+                'description' : '댓글 수정 성공!',
+            },
+        }
+    })  
+    @token_required      
+    def put(self):
+        """댓글 수정"""
+        
+        return {
+            'code' : 200,
+            'message' : '댓글 수정 성공'
+        }
+        
+
+
+
+        
