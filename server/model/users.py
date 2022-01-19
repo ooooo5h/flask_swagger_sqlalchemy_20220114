07ea0db4126b32fc0a046a09d5975f53c593e2bf
전은형 <ooooo5h91@gmail.com>
2022-01-19 16:56:16 +0900

    사용자 암호 password를 password_hashed로 변경함

--- server/model/users.py
+++ server/model/users.py
@@ -12,7 +12,7 @@ class Users(db.Model):
     # 2. 어떤 변수들이 있고, 그 변수들이 어떤 컬럼에 연결되는지 => 변수이름과 컬럼이름은 같아야함
     id = db.Column(db.Integer, primary_key=True)  # id컬럼은 int로 되어있고 기본키라고 명시
     email = db.Column(db.String(50), nullable=False, default='이메일 미입력') # email컬럼은 50자 문구, null허용, 이메일미입력이라는 기본값 있다고 명시
-    password = db.Column(db.String(50), nullable=False)
+    password_hashed = db.Column(db.String(32), nullable=False)
     name = db.Column(db.String(20), nullable=False)
     phone = db.Column(db.String(15))  # nullable의 기본값은 null허용이라서 nullable=False의 경우만 기재해줘도 됨
     
