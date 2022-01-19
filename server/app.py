e0a57fdf0524287e44cdd4fbf2ffa3e1b6d60895
전은형 <ooooo5h91@gmail.com>
2022-01-19 18:00:25 +0900

    /feed/몇번게시글/reply
    주소 양식 변경

--- server/app.py
+++ server/app.py
@@ -33,7 +33,8 @@ def create_app(config_name):
     api.add_resource(Lecture, '/lecture')
     api.add_resource(LectureDetail, '/lecture/<int:lecture_id>')  # /lecture/숫자를 => int로 분석해서 lecture_id에 변수로 담자
     api.add_resource(Feed, '/feed')
-    api.add_resource(FeedReply, '/feed/reply')
+    api.add_resource(FeedReply, '/feed/<int:feed_id>/reply')
+    
     
     # swagger 문서를 자동 생성
     swagger_ui = get_swaggerui_blueprint('/api/docs', '/api/spec.json', config={'app_name' : 'my sns service'})
