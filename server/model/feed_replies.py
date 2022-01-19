1855ab445fd7ab2eb5d027b5a1c9c6612aa1a817
전은형 <ooooo5h91@gmail.com>
2022-01-19 11:39:54 +0900

    댓글과 사용자의 관계 설정
    정석이 아닌 방법을 사용.
    댓글이 작성자가 누구인지를 직접 포함하는 걸로 셋팅

--- server/model/feed_replies.py
+++ server/model/feed_replies.py
@@ -9,7 +9,8 @@ class FeedReplies(db.Model):
     content = db.Column(db.TEXT, nullable=False)      
     created_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
     
-    
+    # 댓글의 입장에서, 나를 적은 사람이 누구인지 들고 있게 하자(정석 X , 자식이 부모가 누군지 변수로 갖자)
+    writer = db.relationship('Users')
     
     def get_data_object(self):
         data = {
@@ -18,6 +19,7 @@ class FeedReplies(db.Model):
             'user_id' : self.user_id,      
             'content' : self.content,      
             'created_at' : str(self.created_at),      
+            'writer' : self.writer.get_data_object(),
         }
                 
         return data
