4c4e1790a47f2d25b212a08d5cca48aff4c19f94
전은형 <ooooo5h91@gmail.com>
2022-01-20 10:49:06 +0900

    강의와 사용자를 연결하는 클래스 추가

--- server/api/admin/dashboard.py
+++ server/api/admin/dashboard.py
@@ -10,6 +10,13 @@ class AdminDashboard(Resource):
         # first()는 한 줄/ all()은 목록 / count()는 검색된 갯수
         users_count = Users.query.filter(Users.email != 'retired').count()
         
+        
+        
+        # 연습 - 자바 강의 수강생의 정보 => JOIN을 썼었는데, ORM으로 어떻게 JOIN을 구현하나 
+        
+        # java_user_list = Users.query.filter()
+        
+        
         return {
             'code' : 200,
             'message' : '임시 응답 : 관리자용 각종 통계 api',
@@ -17,3 +24,5 @@ class AdminDashboard(Resource):
                 'live_user_count' : users_count
             }
         }
+        
+    
---
+++ server/model/lecture_user.py
@@ -0,0 +1,12 @@
+from server import db
+
+class LectureUser(db.Model):
+    
+    __tablename__ = 'lecture_user'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, ForeignKey='users.id')
+    lecture_id = db.Column(db.Integer, ForeignKey='lectures.id')
+    created_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
+    
+    # 굳이 dict로 변경하는 함수는 제작하지 않겠음
