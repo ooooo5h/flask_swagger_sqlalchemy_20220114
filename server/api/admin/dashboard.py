1a895300f2d7ce7f2d082f6d4ea2d8a3357b6c01
전은형 <ooooo5h91@gmail.com>
2022-01-20 12:15:11 +0900

    금액은 아직 미완성, 10일전부터해서 오늘날짜까지 응답에 무조건 추가되게했음.

--- server/api/admin/dashboard.py
+++ server/api/admin/dashboard.py
@@ -60,12 +60,29 @@ class AdminDashboard(Resource):
             
         date_amounts = []
         
-        for row in amount_by_date_list:
+        # 매출이 없는 날은, DB쿼리 결과도 아예 없다 => 목록이 아예 등록이 안됨
+        # 날짜는 무조건 10개를 가져오고, 매출 없는 날은 0원으로 처리해보자
+        
+        # 10일 전 ~ 오늘까지를 for문으로 돌아보기
+        for i in range(0, 11):
+            
+            # 시간/분/초까지 날짜에 들어가니까, 2022-01-11 양식으로 변경해서 넣어주겠다
+ 
             amount_dict = {
-                'date' : str(row[0]),
-                'amount' : int(row[1]),
+                'date' : ten_days_ago.strftime('%Y-%m-%d'),
+                'amount' : 0,
             }
             date_amounts.append(amount_dict)
+
+            # 해당 날짜에서 하루 지난 날로 변경
+            ten_days_ago += datetime.timedelta(days=1)
+        
+        # for row in amount_by_date_list:
+        #     amount_dict = {
+        #         'date' : str(row[0]),
+        #         'amount' : int(row[1]),
+        #     }
+        #     date_amounts.append(amount_dict)
         
         return {
             'code' : 200,
