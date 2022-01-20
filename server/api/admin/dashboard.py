f85e30a59aa1c6cea0be10b7bc361a2f256a56d4
전은형 <ooooo5h91@gmail.com>
2022-01-20 10:21:20 +0900

    탈퇴하지않은 모든 회원수 호출해서, len으로 갯수 파악

--- server/api/admin/dashboard.py
+++ server/api/admin/dashboard.py
@@ -1,9 +1,17 @@
 from flask_restful import Resource
+from server.model import Users
 
 class AdminDashboard(Resource):
     
     def get(self):
+        
+        # 탈퇴안한 회원 수는 몇 명? => users 테이블에서 SELECT문
+        users = Users.query.filter(Users.email != 'retired').all()
+        
         return {
             'code' : 200,
-            'message' : '임시 응답 : 관리자용 각종 통계 api'
+            'message' : '임시 응답 : 관리자용 각종 통계 api',
+            'data' : {
+                'live_user_count' : len(users)
+            }
         }
