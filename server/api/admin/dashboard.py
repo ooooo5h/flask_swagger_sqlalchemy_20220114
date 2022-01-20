0006a3523c256caef0b4bd6a6c5731ba43c46ba0
전은형 <ooooo5h91@gmail.com>
2022-01-20 10:32:40 +0900

    검색 결과의 갯수는 count()함수를 이용하면 더 쉽다

--- server/api/admin/dashboard.py
+++ server/api/admin/dashboard.py
@@ -6,12 +6,14 @@ class AdminDashboard(Resource):
     def get(self):
         
         # 탈퇴안한 회원 수는 몇 명? => users 테이블에서 SELECT문
-        users = Users.query.filter(Users.email != 'retired').all()
+        
+        # first()는 한 줄/ all()은 목록 / count()는 검색된 갯수
+        users_count = Users.query.filter(Users.email != 'retired').count()
         
         return {
             'code' : 200,
             'message' : '임시 응답 : 관리자용 각종 통계 api',
             'data' : {
-                'live_user_count' : len(users)
+                'live_user_count' : users_count
             }
         }
